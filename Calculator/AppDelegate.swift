49701e7285e62db46073f2972a69e1a5a165c313
Clarette Terrasi Díaz <clarette.terrasi@movetia.com>
2020-01-21 14:18:13 +0100

    Create commented version

--- Calculator/AppDelegate.swift
+++ Calculator/AppDelegate.swift
@@ -21,7 +21,7 @@
 // [ ] Agregar paréntesis
 // [ ] Agregar opción de borrar dígitos
 // [ ] Agregar opción de eliminar operación completa
-// [ ] Github
+// [ ] Github commented version
 
 import UIKit
 
